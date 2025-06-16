2864443e8332f137bf6912ec2283c704b73fd39e
Teste123 <b.serip@gmail.com>
2025-06-16 15:10:57 -0300

    primeiro commit

--- 2. ANÁLISE DE DADOS/analise.py
+++ 2. ANÁLISE DE DADOS/analise.py
@@ -1,6 +1,10 @@
+# Primeiro deverá instalar o pandas, abra o terminal e digite:
+# ------>    pip install pandas
+
+
 import pandas as pd
 
-df = pd.read_excel('analise.xlsx')
+df = pd.read_excel('analise_dados.xlsx')
 
 depositos_por_maquina = df['ID MÁQUINA'].value_counts()
 
